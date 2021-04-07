3877e576b88e722af907b9dff770beadd7313ed7
Nick Koenig <nick.koenig.be@gmail.com>
2021-04-07 14:36:26 +0200

    feat(login) login front_end finished

---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="error">
+    <p>hier komt een error</p>
+</div>
+
+    <div class="loginLogo">
+    <img src="./images/logo.PNG" alt="logo">
+    </div>
+
+    <form id="loginForm" action="" method="POST">
+    <label id="loginLabel" for="email">EMAILADRESS</label>
+    <input id="loginInput" type="text" name="email" placeholder="Emailadress">
+
+    <label id="loginLabel" for="password">WACHTWOORD</label>
+    <input id="loginInput"  type="text" name="password" placeholder="Wachtwoord">
+
+    <input id="loginSubmit" type="submit" name="submit">
+    
+    <a href="register.php">Registreren</a>
+
+    </form>
+
+</body>
+</html>
