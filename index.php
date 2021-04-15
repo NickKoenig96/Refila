279161eb687447f583921a0404e97daf08c906fe
Nick Koenig <nick.koenig.be@gmail.com>
2021-04-15 09:58:01 +0200

    feat(index) front-end index finished

---
+++ index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Refila</title>
+</head>
+
+<body>
+    <nav>
+        <img class="logo" src="./images/logo.PNG" alt="logo">
+        <div class="coins">
+            <img src="./images/filamentIcon.svg" alt="your coins">
+            <p> 20.000</p>
+        </div>
+        <img id="navIcon" src="./images/navIcon.svg" alt="navIcon">
+    </nav>
+
+    <div class="line"></div>
+
+    <div class="sectionContainer">
+        <a class="section leaderboard" href="">
+            <div>
+                <p>LEADERBOARD</p>
+            </div>
+        </a>
+        <a class="section stats" href="">
+            <div>
+                <p>STATISTIEKEN</p>
+            </div>
+        </a>
+        <a class="section earn" href="">
+            <div>
+                <p>HOE FILAMENT VERDIENEN</p>
+            </div>
+        </a>
+        <a class="section dMonth" href="">
+            <div>
+                <p>DESIGN VAN DE MAAND</p>
+            </div>
+        </a>
+    </div>
+
+
+
+    <div class="siteSection">
+
+        <h1>Populaire producten</h1>
+
+        <div class="productsIndex">
+
+            <div>
+                <a class="section product" href="">
+                    <div> <img src="./images/filamentProduct.svg" alt="">
+                    </div>
+                </a>
+                <p class="productIndexTitle">Multicolor PLA 3D
+                    Printer Filament</p>
+                <div class="priceProductIndex">
+
+                    <img src="./images/filamentIcon.svg" alt="">
+                    <p>60.000/kg</p>
+                </div>
+            </div>
+
+            <div>
+                <a class="section product" href="">
+                    <div> <img src="./images/filamentProduct.svg" alt="">
+                    </div>
+                </a>
+                <p class="productIndexTitle">Multicolor PLA 3D
+                    Printer Filament</p>
+                <div class="priceProductIndex">
+
+                    <img src="./images/filamentIcon.svg" alt="">
+                    <p>60.000/kg</p>
+                </div>
+            </div>
+
+            <div>
+                <a class="section product" href="">
+                    <div> <img src="./images/filamentProduct.svg" alt="">
+                    </div>
+                </a>
+                <p class="productIndexTitle">Multicolor PLA 3D
+                    Printer Filament</p>
+                <div class="priceProductIndex">
+
+                    <img src="./images/filamentIcon.svg" alt="">
+                    <p>60.000/kg</p>
+                </div>
+            </div>
+
+            <div>
+                <a class="section product" href="">
+                    <div> <img src="./images/filamentProduct.svg" alt="">
+                    </div>
+                </a>
+                <p class="productIndexTitle">Multicolor PLA 3D
+                    Printer Filament</p>
+                <div class="priceProductIndex">
+
+                    <img src="./images/filamentIcon.svg" alt="">
+                    <p>60.000/kg</p>
+                </div>
+            </div>
+
+
+
+
+        </div>
+
+    </div>
+
+
+
+    <div class="siteSection">
+
+        <h1>Aanbevolen voor jou</h1>
+
+        <div class="productsIndex">
+
+            <div>
+                <a class="section product" href="">
+                    <div> <img src="./images/filamentProduct.svg" alt="">
+                    </div>
+                </a>
+                <p class="productIndexTitle">Multicolor PLA 3D
+                    Printer Filament</p>
+                <div class="priceProductIndex">
+
+                    <img src="./images/filamentIcon.svg" alt="">
+                    <p>60.000/kg</p>
+                </div>
+            </div>
+
+            <div>
+                <a class="section product" href="">
+                    <div> <img src="./images/filamentProduct.svg" alt="">
+                    </div>
+                </a>
+                <p class="productIndexTitle">Multicolor PLA 3D
+                    Printer Filament</p>
+                <div class="priceProductIndex">
+
+                    <img src="./images/filamentIcon.svg" alt="">
+                    <p>60.000/kg</p>
+                </div>
+            </div>
+
+            <div>
+                <a class="section product" href="">
+                    <div> <img src="./images/filamentProduct.svg" alt="">
+                    </div>
+                </a>
+                <p class="productIndexTitle">Multicolor PLA 3D
+                    Printer Filament</p>
+                <div class="priceProductIndex">
+
+                    <img src="./images/filamentIcon.svg" alt="">
+                    <p>60.000/kg</p>
+                </div>
+            </div>
+
+            <div>
+                <a class="section product" href="">
+                    <div> <img src="./images/filamentProduct.svg" alt="">
+                    </div>
+                </a>
+                <p class="productIndexTitle">Multicolor PLA 3D
+                    Printer Filament</p>
+                <div class="priceProductIndex">
+
+                    <img src="./images/filamentIcon.svg" alt="">
+                    <p>60.000/kg</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="siteSection">
+        <a id="shopButton" href="">Shop</a>
+    </div>
+
+
+    <footer>
+        <div>
+            <a  href="index.php"><img class="logoFooter" src="./images/logo.PNG" alt="logo"></a>
+            <p>© 2021. All right reserved.</p>
+        </div>
+
+        <div>
+            <h3>PAGINA'S</h3>
+            <div class="footerLinks">
+            <a href="">Orders</a>
+            <a href="">Stats</a>
+            <a href="">Contact</a>
+            </div>
+         
+        </div>
+
+        <div>
+            <h3>CONTACT</h3>
+            <p>contact@refila.be</p>
+            <p>+32 478 35 15 83</p>
+            <a href=""><img src="./images/instagram.png" alt="Instagram"></a>
+            <a href=""><img src="./images/facebook.png" alt="Facebook"></a>
+
+        </div>
+    </footer>
+
+
+
+
+
+</body>
+
+</html>
