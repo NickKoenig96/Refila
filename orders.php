44b907fd459ac98c0d2ebc046367abed4c0f93a6
Nick Koenig <nick.koenig.be@gmail.com>
2021-04-16 15:36:57 +0200

    feat(orders) front-end desktop orders finished

--- orders.php
+++ orders.php
@@ -10,13 +10,13 @@
 </head>
 
 <body>
-<div class="mobileNav">
-<?php include("./nav.inc.php")?>
-</div>  
-<div class="desktopNav">
-  <?php include("./nav2.inc.php")?>
-    </div>  
-<div class="line"></div>
+    <div class="mobileNav">
+        <?php include("./nav.inc.php") ?>
+    </div>
+    <div class="desktopNav">
+        <?php include("./nav2.inc.php") ?>
+    </div>
+    <div class="line"></div>
 
     <div class="ordersContainer">
 
@@ -24,127 +24,131 @@
         <h2>MY Active orders(1)</h2>
         <div>
 
-            <div class="ordersCard">
+            <div class="activeOrders">
+                <div class="ordersCard">
 
-                <div class="orderCardContent">
-                    <h3>OPDRACHTGEVER</h3>
+                    <div class="orderCardContent">
+                        <h3>OPDRACHTGEVER</h3>
 
-                    <div class="ordersPerson">
-                        <img src="./images/Profile.jpeg" alt="">
-                        <p>Carlton_Brasserie</p>
-                    </div>
-
-                    <div>
-                        <h3>BESCHRIJVING</h3>
-                        <p>plastick vork (wit)</p>
-                    </div>
-
-                    <div class="ordersInfo">
-                        <div>
-                            <h3>AANTAL</h3>
-                            <p>75</p>
+                        <div class="ordersPerson">
+                            <img src="./images/Profile.jpeg" alt="">
+                            <p>Carlton_Brasserie</p>
                         </div>
 
                         <div>
-                            <h3>DEADLINE</h3>
-                            <p>5 apr 2021</p>
+                            <h3>BESCHRIJVING</h3>
+                            <p>plastick vork (wit)</p>
                         </div>
-                    </div>
+
+                        <div class="ordersInfo">
+                            <div>
+                                <h3>AANTAL</h3>
+                                <p>75</p>
+                            </div>
+
+                            <div>
+                                <h3>DEADLINE</h3>
+                                <p>5 apr 2021</p>
+                            </div>
+                        </div>
 
 
-                    <div>
-                        <h3>OPHALING/VERZENDING</h3>
-                        <p>Ophaling</p>
-                    </div>
-
-                    <div class="orderStatus">
                         <div>
-                            <form action="" method="POST">
-                                <input type="hidden" name="ordersSubmit" value="3487">
-                                <input class="ordersSubmit" type="submit" value="Markeren als klaar">
-                            </form>
+                            <h3>OPHALING/VERZENDING</h3>
+                            <p>Ophaling</p>
                         </div>
 
-                        <div class="coinsOrders">
-                            <img src="./images/filamentIcon.svg" alt="your coins">
-                            <p> 20.000</p>
-                        </div>
+                        <div class="orderStatus">
+                            <div>
+                                <form action="" method="POST">
+                                    <input type="hidden" name="ordersSubmit" value="3487">
+                                    <input class="ordersSubmit" type="submit" value="Markeren als klaar">
+                                </form>
+                            </div>
 
-                        <div class="mailOrders">
-                            <p><a href="mailto:someone@example.com">Contact opnemen</a></p>
-                            <p>emailadress here</p>
-                        </div>
+                            <div class="coinsOrders">
+                                <img src="./images/filamentIcon.svg" alt="your coins">
+                                <p> 20.000</p>
+                            </div>
 
+                            <div class="mailOrders">
+                                <p><a href="mailto:someone@example.com">Contact opnemen</a></p>
+                                <p>emailadress here</p>
+                            </div>
+
+
+                        </div>
 
                     </div>
+
+
+
 
                 </div>
 
 
 
 
-            </div>
+                <div class="ordersCard">
 
+                    <div class="orderCardContent">
+                        <h3>OPDRACHTGEVER</h3>
 
-
-
-            <div class="ordersCard">
-
-                <div class="orderCardContent">
-                    <h3>OPDRACHTGEVER</h3>
-
-                    <div class="ordersPerson">
-                        <img src="./images/Profile.jpeg" alt="">
-                        <p>Carlton_Brasserie</p>
-                    </div>
-
-                    <div>
-                        <h3>BESCHRIJVING</h3>
-                        <p>plastick vork (wit)</p>
-                    </div>
-
-                    <div class="ordersInfo">
-                        <div>
-                            <h3>AANTAL</h3>
-                            <p>75</p>
+                        <div class="ordersPerson">
+                            <img src="./images/Profile.jpeg" alt="">
+                            <p>Carlton_Brasserie</p>
                         </div>
 
                         <div>
-                            <h3>DEADLINE</h3>
-                            <p>5 apr 2021</p>
+                            <h3>BESCHRIJVING</h3>
+                            <p>plastick vork (wit)</p>
                         </div>
-                    </div>
+
+                        <div class="ordersInfo">
+                            <div>
+                                <h3>AANTAL</h3>
+                                <p>75</p>
+                            </div>
+
+                            <div>
+                                <h3>DEADLINE</h3>
+                                <p>5 apr 2021</p>
+                            </div>
+                        </div>
 
 
-                    <div>
-                        <h3>OPHALING/VERZENDING</h3>
-                        <p>Ophaling</p>
-                    </div>
-
-                    <div class="orderStatus">
                         <div>
-                            <form action="" method="POST">
-                                <input type="hidden" name="ordersSubmit" value="3487">
-                                <input class="ordersSubmit" type="submit" value="Markeren als klaar">
-                            </form>
+                            <h3>OPHALING/VERZENDING</h3>
+                            <p>Ophaling</p>
                         </div>
 
-                        <div class="coinsOrders">
-                            <img src="./images/filamentIcon.svg" alt="your coins">
-                            <p> 20.000</p>
-                        </div>
+                        <div class="orderStatus">
+                            <div>
+                                <form action="" method="POST">
+                                    <input type="hidden" name="ordersSubmit" value="3487">
+                                    <input class="ordersSubmit" type="submit" value="Markeren als klaar">
+                                </form>
+                            </div>
 
-                        <div class="mailOrders">
-                            <p><a href="mailto:someone@example.com">Contact opnemen</a></p>
-                            <p>emailadress here</p>
-                        </div>
+                            <div class="coinsOrders">
+                                <img src="./images/filamentIcon.svg" alt="your coins">
+                                <p> 20.000</p>
+                            </div>
 
+                            <div class="mailOrders">
+                                <p><a href="mailto:someone@example.com">Contact opnemen</a></p>
+                                <p>emailadress here</p>
+                            </div>
+
+
+                        </div>
 
                     </div>
 
                 </div>
-
             </div>
+
+
         </div>
 
 
@@ -221,7 +225,7 @@
 
 
             </div>
-    </div>
+        </div>
 
 
 
@@ -231,7 +235,7 @@
 
 
 
-    <h2>My completed orders</h2>
+        <h2>My completed orders</h2>
         <div>
 
             <div class="ordersCard">
@@ -271,7 +275,7 @@
                         <div>
                             <form action="" method="POST">
                                 <input type="hidden" name="ordersSubmit" value="3487">
-                                <input class="ordersSubmit ordersComplete"  value="klaar">
+                                <input class="ordersSubmit ordersComplete" value="klaar">
                             </form>
                         </div>
 
@@ -295,17 +299,17 @@
 
             </div>
 
+        </div>
+
+
+
+
+
+
     </div>
-
-
-
-
-
-
-</div>
-<div class="footer">
-<?php include("./footer.inc.php")?>
-</div>
+    <div class="footer">
+        <?php include("./footer.inc.php") ?>
+    </div>
 
 </body>
 
