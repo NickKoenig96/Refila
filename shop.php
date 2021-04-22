2c657a1f68880391592e6384c6ca5bf6eabc5d65
Nick Koenig <nick.koenig.be@gmail.com>
2021-04-22 11:55:49 +0200

    feat(shop) shop front-end mobile finished

---
+++ shop.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Refila</title>
+</head>
+
+<body>
+
+    <div class="mobileNav">
+        <?php include("./nav.inc.php") ?>
+    </div>
+
+    <div class="desktopNav">
+        <?php include("./nav2.inc.php") ?>
+    </div>
+
+    <div class="line"></div>
+
+    <div class="shop">
+        <h3 class="shopH3">Popoluaire producten</h3>
+
+        <div class="shopContainer">
+
+            <div class="shopProduct">
+                <div class="shopProductImage">
+                    <img src="./images/filamentProduct.svg" alt="filament">
+                </div>
+                <p>Multicolor PLA 3D Printer Filament</p>
+                <div class="shopFilament">
+                    <img src="./images/filamentIcon.svg" alt="filamentIcon">
+                    <p>60.00/kg</p>
+                </div>
+            </div>
+
+            <div class="shopProduct">
+                <div class="shopProductImage">
+                    <img src="./images/filamentProduct.svg" alt="filament">
+                </div>
+                <p>Multicolor PLA 3D Printer Filament</p>
+                <div class="shopFilament">
+                    <img src="./images/filamentIcon.svg" alt="filamentIcon">
+                    <p>60.00/kg</p>
+                </div>
+            </div>
+
+        </div>
+
+
+        <h3 class="shopH3">Aanbevolen voor jou</h3>
+
+        <div class="shopContainer">
+
+            <div class="shopProduct">
+                <div class="shopProductImage">
+                    <img src="./images/filamentProduct.svg" alt="filament">
+                </div>
+                <p>Multicolor PLA 3D Printer Filament</p>
+                <div class="shopFilament">
+                    <img src="./images/filamentIcon.svg" alt="filamentIcon">
+                    <p>60.00/kg</p>
+                </div>
+            </div>
+
+            <div class="shopProduct">
+                <div class="shopProductImage">
+                    <img src="./images/filamentProduct.svg" alt="filament">
+                </div>
+                <p>Multicolor PLA 3D Printer Filament</p>
+                <div class="shopFilament">
+                    <img src="./images/filamentIcon.svg" alt="filamentIcon">
+                    <p>60.00/kg</p>
+                </div>
+            </div>
+
+        </div>
+
+
+        <h3 class="shopH3">Alle producten</h3>
+
+        <div class="shopContainer">
+
+            <div class="shopProduct">
+                <div class="shopProductImage">
+                    <img src="./images/filamentProduct.svg" alt="filament">
+                </div>
+                <p>Multicolor PLA 3D Printer Filament</p>
+                <div class="shopFilament">
+                    <img src="./images/filamentIcon.svg" alt="filamentIcon">
+                    <p>60.00/kg</p>
+                </div>
+            </div>
+
+            <div class="shopProduct">
+                <div class="shopProductImage">
+                    <img src="./images/filamentProduct.svg" alt="filament">
+                </div>
+                <p>Multicolor PLA 3D Printer Filament</p>
+                <div class="shopFilament">
+                    <img src="./images/filamentIcon.svg" alt="filamentIcon">
+                    <p>60.00/kg</p>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+    </div>
+
+</body>
+
+</html>
