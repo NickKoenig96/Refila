449991d90ff7bcb9a6ddbd9385f9609aaa978182
Nick Koenig <nick.koenig.be@gmail.com>
2021-05-19 16:42:22 +0200

    feat(landingpage) add landingpage

---
+++ landingpage.php
@@ -0,0 +1,461 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style2.css">
+
+    <title>Refila</title>
+</head>
+
+<body>
+    <nav>
+        <img class="logoL" src="./images/logo.svg" alt="logo">
+    </nav>
+
+    <div class="landinpageSection">
+        <div>
+            <h1>Plastic afval omzetten tot filament voor de horeca</h1>
+            <p>Bij Refila zetten we plastic afval van de Mechelse horecazaken om tot 3d print
+                filament. Dit filament kunnen de mensen met een 3d printer dan weer gebruiken
+                om allerlei zaken te printen die de horeca kan gebruiken, zoals decoratie of zelfs
+                bestek. De mensen met een 3d printer kunnen punten verdienen als ze iets
+                maken voor de horecazaken en kunnen met deze punten zelf filament van ons
+                kopen om zelf creaties te maken.</p>
+            <div id="button">
+                <a href="index.php">ga naar het Refila platform</a>
+
+            </div>
+        </div>
+
+        <div>
+            <img class='laptopL' src="./images/mockup1.jpg" alt="pc">
+        </div>
+
+    </div>
+
+    <div class="landinpageSection --gallery">
+        <img class="bigImageL" src="./images/fullwidthimage.jpg" alt="">
+    </div>
+
+    <div class="landinpageSection">
+
+        <div>
+            <img class='laptopL L--2' src="./images/mockup2.jpg" alt="pc">
+        </div>
+
+
+        <div>
+            <h1>Plastic afval omzetten tot filament voor de horeca</h1>
+            <p>Bij Refila zetten we plastic afval van de Mechelse horecazaken om tot 3d print
+                filament. Dit filament kunnen de mensen met een 3d printer dan weer gebruiken
+                om allerlei zaken te printen die de horeca kan gebruiken, zoals decoratie of zelfs
+                bestek. De mensen met een 3d printer kunnen punten verdienen als ze iets
+                maken voor de horecazaken en kunnen met deze punten zelf filament van ons
+                kopen om zelf creaties te maken.</p>
+
+        </div>
+
+
+    </div>
+
+    <div class="landinpageSection ">
+
+        <div>
+            <h1 class='featuresTitle'>Features voor 3D printers</h1>
+
+            <div class="features">
+                <div class="feature">
+                    <h3>Bestelingen</h3>
+                    <p>
+                        Ze krijgen een overzicht van alle geplaatste
+                        bestellingen op het platform en kunnen zo
+                        kiezen welke ze gaan uitvoeren en welke niet.
+                    </p>
+                </div>
+                <div class="feature">
+                    <h3>Filament verdienen</h3>
+                    <p>
+                        De filament punten die ze verdienen kunnen ze
+                        gebruiken om in de shop filament te kopen die
+                        ze thuisgeleverd krijgen.
+                    </p>
+                </div>
+                <div class="feature">
+                    <h3>Statistieken bekijken</h3>
+                    <p>
+                        Ze kunnen hier zien hoeveel bestellingen ze
+                        al voltooid hebben en hoeveel filament ze in
+                        totaal hebben verdiend.
+                    </p>
+                </div>
+
+            </div>
+
+            <div class="features">
+                <div class="feature">
+                    <h3>Leaderboard</h3>
+                    <p>
+                        Hoe meer punten je verdient, hoe hoger je komt
+                        op het leaderboard. De top 1 kan een eigen
+                        ontwerp op de website zetten.
+                    </p>
+                </div>
+                <div class="feature">
+                    <h3>Design van de maand</h3>
+                    <p>
+                        Design van de maand is het design dat gemaakt
+                        is door de persoon die de vorige maand eerste
+                        stond op het leaderboard.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+
+
+
+    </div>
+
+    <div class="landinpageSection ">
+
+        <div>
+            <h1 class='featuresTitle'>Features voor Horecazaken</h1>
+
+            <div class="features">
+                <div class="feature">
+                    <h3>Bestelingen</h3>
+                    <p>
+                        Gebruikers kunnen bestellingen plaatsen
+                        die mensen met een 3d printer vervolgens
+                        kunnen aannemenen uitvoeren.
+                    </p>
+                </div>
+                <div class="feature">
+                    <h3>Filament verdienen</h3>
+                    <p>
+                        Hoe meer plastic afval je verzamelt, hoe meer
+                        punten je krijgt op ons platform. Met deze
+                        punten kan je dingen kopen in de shop.
+                    </p>
+                </div>
+                <div class="feature">
+                    <h3>Statistieken bekijken</h3>
+                    <p>
+                        Een mooi overzicht waar je kan zien hoeveel
+                        kilogram plastic afval je al hebt verzamelt.
+                    </p>
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+    </div>
+
+    <div class="landinpageSection --gallery">
+        <img class="bigImageL bigImageL2" src="./images/mockup_web.png" alt="">
+    </div>
+
+    <div class="landinpageSection">
+        <div class="partnerImages">
+            <h1>Onze Partners</h1>
+            <div class="test">
+                <img src="./images/mechelen.png" alt="">
+                <img src="./images/thomasmore.png" alt="">
+            </div>
+
+            <p>Dit idee was één van de 3 winnende ideeën tijdens de hackathon
+                georganiseerd door Stad Mechelen.</p>
+        </div>
+
+    </div>
+
+    <div class="landinpageSection">
+        <div class="partnerImages">
+            <h1>Klanten</h1>
+            <div class="klanten">
+                <img src="./images/het_anker.png" alt="">
+                <img src="./images/il_cardinale.png" alt="">
+                <img src="./images/pastorale.png" alt="">
+                <img src="./images/puro.png" alt="">
+                <img src="./images/tinelle.png" alt="">
+            </div>
+        </div>
+
+    </div>
+
+
+
+    <div class="landinpageSection">
+        <div>
+            <h1>Abonnementen horeca</h1>
+            <div class=abbonementen>
+                <div class="abbo feature">
+
+                    <h3>Horeca LV1</h3>
+                    <ul>
+                        <li>- 1399 filament punten per maand</li>
+                        <li>- bestelling komt hoger in de lijst dan die van gebruikers zonder abonnement</li>
+                    </ul>
+                    <button>$500,-</button>
+                </div>
+
+                <div class="abbo feature">
+                    <h3>Horeca LV2</h3>
+                    <ul>
+                        <li>- 2899 filament punten per maand</li>
+                        <li>- bestelling komt hoger in de lijst dan die van LV1 gebruikers</li>
+                    </ul>
+                    <button>$1000,-</button>
+                </div>
+
+                <div class="abbo feature">
+                    <h3>Horeca LV3</h3>
+                    <ul>
+                        <li>- 7299 filament punten per maand</li>
+                        <li>- bestelling komt hoger in de lijst dan die van LV2 gebruikers</li>
+                    </ul>
+                    <button>$2500,-</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+
+
+
+
+
+
+    <div class="landinpageSection L--abbo">
+
+        <div class="abbonementen2">
+            <h1>Abonnementen 3d printers</h1>
+
+            <div class="abbonementen ">
+                <div class="abbo feature">
+
+                    <h3>The 3D dude</h3>
+                    <ul>
+                        <li>- 35 opdrachten aannemen</li>
+                    </ul>
+                    <button>$99,-</button>
+                </div>
+
+                <div class="abbo feature">
+                    <h3>The 3D hero</h3>
+                    <ul>
+                        <li>- 20 opdrachten aannemen</li>
+                    </ul>
+                    <button>$259,-</button>
+                </div>
+
+                <div class="abbo feature">
+                    <h3>The 3D maniac</h3>
+                    <ul>
+                        <li>- 50 opdrachten aannemen</li>
+                    </ul>
+                    <button>$499,-</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="ordersContainer">
+
+        <h4>Stats</h2>
+
+            <div class="statsCard">
+                <div class="yourStats">
+                    <h3 class="statsH3">AANTAL FILAMENT VERDIEND:</h3>
+                    <div class="statsCoins">
+                        <img src="./images/filamentIcon.svg" alt="filamentIcon">
+                        <p class="statsCoins">11060.00</p>
+                    </div>
+                    <div class="statsLine"></div>
+                </div>
+                <div class="yourStats">
+                    <h3 class="statsH3">AANTAL ORDERS VOLTOOID:</h3>
+                    <div class="statsCoins">
+                        <img src="./images/filamentIcon.svg" alt="filamentIcon">
+                        <p class="statsCoins">10723.8</p>
+                        < </div>
+                            <div class="statsLine"></div>
+                    </div>
+                    <div class="yourStats">
+                        <h3 class="statsH3">GROOTSTE ORDER UITGAVE</h3>
+                        <div class="statsCoins">
+                            <img src="./images/filamentIcon.svg" alt="filamentIcon">
+
+                            <p class="statsCoins">1603.1</p>
+                        </div>
+                        <div class="statsLine"></div>
+                    </div>
+                    <div class="yourStats">
+                        <h3 class="statsH3">AANTAL FILAMENT BETAALD <?php echo date(" F ") ?>:</h3>
+                        <div class="statsCoins">
+                            <img src="./images/filamentIcon.svg" alt="filamentIcon">
+                            <p class="statsCoins">10794.5</p>
+                        </div>
+                        <div class="statsLine"></div>
+                    </div>
+                    <div class="yourStats">
+                        <h3 class="statsH3">AANTAL Orders FEB VOLTOOID:</h3>
+                        <div class="statsCoins">
+                            <img src="./images/filamentIcon.svg" alt="filamentIcon">
+                            <p class="statsCoins">1334.8</p>
+                        </div>
+                        <div class="statsLine"></div>
+                    </div>
+                    <div class="yourStats">
+                        <h3 class="statsH3">GROOTSTE ORDER UITGAVE FEB:</h3>
+                        <div class="statsCoins">
+                            <img src="./images/filamentIcon.svg" alt="filamentIcon">
+                            <p class="statsCoins">1009.5</p>
+                        </div>
+                        <div class="statsLine"></div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="landinpageSection l--newsletter ">
+            <h1 class="newsletterH1">Schrijf je in voor onze nieuwsbrief</h1>
+            <div class="newsletterBody">
+            <div >
+                <p>Blijf op de hoogte van onze nieuwste updates, features en
+                allerlei andere zaken die je zeker niet wilt missen!</p>
+                <div>
+                    <input type="text" placeholder="Emailadres">
+                    <button class="btn_newsletter">Bevestig</button>
+
+
+                </div>
+                </div>
+            </div>
+               
+                
+              
+            </div>
+
+
+
+            <div class="ordersContainer">
+                <h2>Contact</h2>
+                <div class="ordersCard orderscard2">
+
+                    <div class="contactCard">
+                        <div class="contact">
+                            <form id="contactForm" action="" method="POST">
+                                <label id="loginLabel " for="surname">VOORNAAM</label>
+                                <input class="contactInput" type="text" name="surname" placeholder="Voornaam">
+
+                                <label id="loginLabel" for="name">NAAM</label>
+                                <input class="contactInput" type="text" name="name" placeholder="Naam">
+
+                                <label id="loginLabel" for="email">EMAILADRESS</label>
+                                <input class="contactInput" type="text" name="email" placeholder="Emailadress">
+
+                                <label id="loginLabel" for="bericht">BERICHT</label>
+                                <textarea name="bericht" form="contactForm" placeholder="Bericht"></textarea>
+
+                                <input id="contactButton" name="submit" value="send">
+
+                            </form>
+
+                        </div>
+
+                        <div class="contactInfoDesktop">
+
+
+
+                            <div class="contactInfoDiv">
+                                <img src="./images/telephone.svg" alt="phoneicon">
+                                <p>+32 478 35 15 83</p>
+                            </div>
+
+                            <div class="contactInfoDiv">
+                                <img src="./images/location.svg" alt="locationicon">
+                                <p>2800, Mechelen</p>
+                            </div>
+
+                            <div class="contactInfoDiv">
+                                <img src="./images/email.svg" alt="emailicon">
+                                <p>contact@refila.be</p>
+                            </div>
+
+
+
+
+
+                            <div class="contactInfoDiv">
+                                <img src="./images/instagram.svg" alt="instagramicon">
+                                <img src="./images/facebook.svg" alt="facebookicon">
+                            </div>
+
+
+
+                        </div>
+
+                    </div>
+
+
+
+
+
+
+                </div>
+
+                <div class="contactInfo">
+
+
+
+                    <div class="contactInfoDiv">
+                        <img src="./images/telephone.svg" alt="phoneicon">
+                        <p>+32 478 35 15 83</p>
+                    </div>
+
+                    <div class="contactInfoDiv">
+                        <img src="./images/location.svg" alt="locationicon">
+                        <p>2800, Mechelen</p>
+                    </div>
+
+                    <div class="contactInfoDiv">
+                        <img src="./images/email.svg" alt="emailicon">
+                        <p>contact@refila.be</p>
+                    </div>
+
+
+
+
+
+                    <div class="contactInfoDiv">
+                        <img src="./images/instagram.svg" alt="instagramicon">
+                        <img src="./images/facebook.svg" alt="facebookicon">
+                    </div>
+
+
+
+                </div>
+            </div>
+
+            <div class="footer">
+                <?php include("./footer.inc.php") ?>
+            </div>
+
+
+
+</body>
+
+</html>
